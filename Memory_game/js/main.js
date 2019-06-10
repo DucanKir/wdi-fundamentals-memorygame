69a90356d976abc8ef652511066eeae53f7166a8
Admin <admin@Admins-MacBook-Air.local>
2019-06-10 17:05:38 +0100

    game logic

--- Memory_game/js/main.js
+++ Memory_game/js/main.js
@@ -1,18 +1,24 @@
 var cards = ["queen", "queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[1];
-var cardTwo = cards[3];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
+function checkForMatch(){
 if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0]===cardsInPlay[1]) {
-		alert("You found a match!")
-	} else {
-		alert("Sorry, try again.")
+		if (cardsInPlay[0]===cardsInPlay[1]) {
+		alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
 	}
 }
+}
+function flipCard (cardId){
 
-console.log("Flipped "+cardsInPlay);
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	checkForMatch()
+	}
+
+flipCard(0);
+flipCard(2);
+
+
 
